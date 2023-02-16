e543b583bdef0c4d85bec16da4d8abe4a8807171
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-02-16 10:26:17 -0800

    Hello using puts function.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*string initialisation*/
+	char Message[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Message); /*writing the string to stdout*/
+	return (0);
+}
