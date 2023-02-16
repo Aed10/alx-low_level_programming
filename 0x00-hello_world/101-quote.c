fff3fbc4bcc2fc9f1c394103f47aaec9720de933
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-02-16 13:20:31 -0800

    Errstd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Error 1 (0, Success)
+ */
+int main(void)
+{	/*Print in Error_Standard*/
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
