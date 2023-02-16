1536d086e4fcbfd3499aece295450875e4941136
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-02-16 10:39:23 -0800

    printf now

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{	/* Printf to print a string in the output std. */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* return 0 if all is ok */
+}
