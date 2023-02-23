c17c56cfadf70a3e3cf69cde408f84012bf4845d
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-02-23 11:28:13 -0800

    Product is easy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - is function to calculate the product of two integers.
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the product of @a and @b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
