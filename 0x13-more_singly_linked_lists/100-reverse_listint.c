083a102a3281cb4d2469dc0dde11496d9ab291cb
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-04 15:51:57 -0700

    Update 0x13-more_singly_linked_lists - Print a list in a safe manner

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,33 @@
  * Return: Pointer to the new head.
  */
 
-listint_t *reverse_listint(listint_t **head)
+size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *previous, *next_node;
+	size_t count = 0;
 
-	previous = NULL;
-	while ((*head) != NULL)
-	{
-		next_node = (*head)->next;
-		(*head)->next = previous;
-		previous = *head;
-		*head = next_node;
-	}
-	*head = previous;
-	return (*head);
+    if (head == NULL) {
+        printf("Error: head is NULL\n");
+        exit (98);
+    }
+
+    const listint_t *slow = head;
+    const listint_t *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next; // move slow pointer by 1 step
+        fast = fast->next->next; // move fast pointer by 2 steps
+
+        // If fast and slow pointers meet, it indicates a cycle
+        if (slow == fast) {
+            printf("Error: Linked list contains a cycle\n");
+            exit (98); // or return an appropriate error code
+        }
+
+        // Print current node's address and value
+        printf("[%p] %d\n", (void*)slow, slow->n);
+	count ++;
+    }
+
+    return (count); // or return the appropriate count if needed
 }
+
