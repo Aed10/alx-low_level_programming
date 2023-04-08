1450582a7d3dd01c6bb4b1a7278e6a4aaa524917
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-07 17:27:03 -0700

    Update 1-print_binary.c - Unsigned long 64 bit.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 	int i;
 
 	/* Determine the psition of the most significant non-zero bit */
-	for (i = 31; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
 			break;
