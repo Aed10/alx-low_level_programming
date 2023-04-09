e6b5ca2eb4d3778ae4c6cffa9b0474a634624085
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-09 08:13:03 -0700

    Update 1-create_file.c -

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,13 +11,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int fd, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	fd = creat(filename, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -25,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len] != '\0'; len++)
 			;/* Get the length of the text_content. */
 
-		if (write(fd, text_content, len) == -1)
+		if (write(fd, text_content, len + 1) == -1)
 		return (-1);
 
 	}
