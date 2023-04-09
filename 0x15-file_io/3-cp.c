0f6a1a239d1cbc63d5fa94e58e8889ace7b00957
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-09 16:42:55 -0700

    Update 3-cp.c -

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,8 +12,8 @@
 */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, nbread;
-	char *file_from, *file_to;
+	int fd_from, fd_to, nbread, nbwrote;
+	char *file_from = av[1], *file_to = av[2];
 	char buffer[1024];
 
 	if (ac != 3)
@@ -21,9 +21,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(1);
 	}
-
-	file_from = av[1];
-	file_to = av[2];
 
 	fd_from = open(file_from, O_RDONLY);
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -35,20 +32,21 @@ int main(int ac, char **av)
 
 	while ((nbread = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		if (write(fd_to, buffer, nbread) != nbread)
+		nbwrote = write(fd_to, buffer, nbread);
+		if (nbwrote != nbread ||  nbwrote == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
-	if (close(fd_from) == -1)
+	if (nbread == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd  %i", fd_from);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
-	if (close(fd_to) == -1)
+	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd  %i", fd_to);
+		dprintf(2, "Error: Can't close fd  %i", (fd_from == -1) ? fd_from : fd_to);
 		exit(100);
 	}
 	return (0);
