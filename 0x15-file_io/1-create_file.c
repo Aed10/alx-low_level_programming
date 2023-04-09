c655e0b42e76321e4bc480f321ab7fdfb8ea422d
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-09 08:21:34 -0700

    Update 1-create_file.c - Fix open_flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len] != '\0'; len++)
 			;/* Get the length of the text_content. */
 
-		if (write(fd, text_content, len + 1) == -1)
+		if (write(fd, text_content, len) == -1)
 		return (-1);
 
 	}
