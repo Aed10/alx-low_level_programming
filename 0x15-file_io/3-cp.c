e15ff601c5011929e8b06157e3af34f24564961e
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-09 18:28:41 -0700

    Update 3-cp.c - Added new line in close case.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,12 +40,12 @@ int main(int ac, char **av)
 	}
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	return (0);
