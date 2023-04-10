efa6d9f185122dd809c397e7fd0fe6ac901c1855
ED DYYANY AYOUB <eddyyanyayoub@gmail.com>
2023-04-10 12:34:01 -0700

    Update 0x15-file_io -

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(1);
+		exit(97);
 	}
 	fd_from = open(file_from, O_RDONLY);
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
