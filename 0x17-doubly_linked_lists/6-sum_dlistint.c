20979925c67afa694273025355a59eb1388e4501
Aed10 <eddyyanyayoub@gmail.com>
2023-05-18 22:21:35 +0100

    "Refactor the sum_dlistint function to handle empty lists by returning 0."

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,7 +5,7 @@
  * all the data (n) of a dlistint_t linked list.
  * @head: pointer to the head of the list
  * Return: sum of all the data (n) of a dlistint_t linked list,
-	or 0 if the list is empty
+ * or 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
