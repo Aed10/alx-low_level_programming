8d613441139964229ddb6fc4d3a1b9d531629963
Aed10 <eddyyanyayoub@gmail.com>
2023-09-08 22:53:38 +0100

    Refactor hash_table_print and add hash_table_delete function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,68 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Print a given hash table.
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
  *
- * @ht: A given hash table.
- *
- * Return: Nothing.
+ * Return: void
  */
-
-unsigned long int	hash_table_count(const hash_table_t *ht);
 void	hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *tmp;
 	unsigned long int i;
-	unsigned long int count;
-	hash_node_t *node;
+	char *sep;
 
-	count = 0;
+	tmp = NULL;
+	i = 0;
+	sep = "";
 	if (!ht)
-	{
-		printf("{}\n"); /* Handle the case when the hash table is NULL*/
 		return;
-	}
 	printf("{");
-	for (i = 0; i < ht->size; i++)
+	while (i < ht->size)
 	{
-		node = ht->array[i];
-		while (node != NULL)
+		tmp = ht->array[i];
+		while (tmp)
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			count++;
-			if (count < hash_table_count(ht))
-			{
-				printf(", ");
-					/* Add a comma and space if there are more key-value pairs */
-			}
-			node = node->next;
+			printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
+			sep = ", ";
+			tmp = tmp->next;
 		}
+		i++;
 	}
 	printf("}\n");
-}
-
-/**
- * hash_table_count - Count the number of key-value pairs in a hash table.
- *
- * @ht: A given hash table.
- *
- * Return: The number of key-value pairs in the hash table.
- */
-
-unsigned long int hash_table_count(const hash_table_t *ht)
-{
-	hash_node_t *node;
-	unsigned long int i;
-	unsigned long int count;
-
-	count = 0;
-	for (i = 0; i < ht->size; i++)
-	{
-		node = ht->array[i];
-		while (node != NULL)
-		{
-			count++;
-			node = node->next;
-		}
-	}
-	return (count);
 }
