108cc12d042043c22b10c936ea32aaf6e95e897a
Aed10 <eddyyanyayoub@gmail.com>
2023-09-08 18:58:45 +0100

    Refactor hash_tables.h for improved readability and organization

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,8 @@
 #ifndef HASH_TABLES_H
-#define HASH_TABLES_H
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+# define HASH_TABLES_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -15,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char	*key;
+	char	*value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,9 +30,10 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
+/** ----- Functions --------**/
+hash_table_t	*hash_table_create(unsigned long int size);
 #endif
